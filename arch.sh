52e5dec60fe19cf27c1ed1e61ae40664fe732f26
gamb1t <74240139+gamb2t@users.noreply.github.com>
2022-11-18 10:34:47 +0900

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -3,6 +3,6 @@ apt update
 apt install grub-imageboot -y
 mkdir /boot/images
 cd /boot/images
-wget https://
+wget http://mirror.fcix.net/archlinux/iso/2022.11.01/archlinux-2022.11.01-x86_64.iso
 update-grub2
 
